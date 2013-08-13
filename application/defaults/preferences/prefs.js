79c6666b0fd62aa60d1d25d3c4c7b67b9f8942f4
Ye Chuah <yechuah@gmail.com>
2013-08-13 20:06:37 +1000

    download and alert prefs

--- application/defaults/preferences/prefs.js
+++ application/defaults/preferences/prefs.js
@@ -12,3 +12,21 @@ pref("javascript.options.strict", true);
 pref("nglayout.debug.disable_xul_cache", true);
 pref("nglayout.debug.disable_xul_fastload", true);
 pref("security.warn_viewing_mixed", false);
+
+pref("browser.download.useDownloadDir", true);
+pref("browser.download.defaultFolder", "/tmp");
+pref("browser.download.folderList", 1);
+pref("browser.download.manager.showAlertOnComplete", false);
+pref("browser.download.manager.showAlertInterval", 2000);
+pref("browser.download.manager.retention", 0);
+pref("browser.download.manager.showWhenStarting", false);
+pref("browser.download.manager.useWindow", false);
+pref("browser.download.manager.closeWhenDone", true);
+pref("browser.download.manager.openDelay", 0);
+pref("browser.download.manager.focusWhenStarting", false);
+pref("browser.download.manager.flashCount", 0);
+
+pref("alerts.slideIncrement", 1);
+pref("alerts.slideIncrementTime", 10);
+pref("alerts.totalOpenTime", 4000);
+pref("alerts.height", 50);
